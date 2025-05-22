c7e237652f2a92d227109ca7b778e789f35ebbd2
asayed82 <asayed82@gmail.com>
2025-05-22 21:08:32 +0400

    remote my-adk-samples folder

--- my-sample-agents/server/core/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    #filename="debug_messages.log",
-    level=logging.INFO, #logging.DEBUG, 
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-)
-
-logger = logging.getLogger(__name__)
